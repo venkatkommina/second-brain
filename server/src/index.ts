136a2db543a8191a971473f79634b4928ccd31ea
Venkat K <venkatkommina91@gmail.com>
2025-05-29 12:11:54 +0530

    Setup and start backend server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import router from "./routes";
 import { connectToDB } from "./db";
+import cors from "cors";
 
 const port = 5000;
-
 const app = express();
 
+app.use(cors());
 app.use("/api/v1", router);
 
 async function startServer() {
