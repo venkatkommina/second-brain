69b12f8acf41156775d88cd3981546cb933780e6
venkatkommina <venkatkommina91@gmail.com>
2025-07-25 23:13:13 +0530

    callback_uri

--- server/src/passport.ts
+++ server/src/passport.ts
@@ -15,7 +15,10 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `/api/v1/auth/google/callback`,
+        callbackURL:
+          process.env.NODE_ENV === "production"
+            ? `${process.env.SERVER_URL}/api/v1/auth/google/callback`
+            : "http://localhost:3001/api/v1/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
